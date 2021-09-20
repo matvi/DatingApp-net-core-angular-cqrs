03a4e32a78e65e3d05d15bf612baceb73b830bb2
matvi <ing.david.mata@gmail.com>
2021-09-20 02:13:24 -0500

    Refactoring ValidationBehaviour to handle FluentResults

--- Common/PipelinesBehaviours/ValidationBehaviour.cs
+++ Common/PipelinesBehaviours/ValidationBehaviour.cs
@@ -2,13 +2,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using FluentResults;
 using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
 
 namespace Common.PipelinesBehaviours
 {
-    public class ValidationBehaviour<TRequest,TResponse> : IPipelineBehavior<TRequest,TResponse>
-        where TRequest: IRequest<TResponse>
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TResponse : new()
+
     {
         private readonly IEnumerable<IValidator<TRequest>> _validators;
 
@@ -16,9 +20,11 @@ namespace Common.PipelinesBehaviours
         {
             _validators = validators;
         }
-        
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
         {
+            var response = new TResponse();
             if (_validators.Any())
             {
                 var context = new ValidationContext<TRequest>(request);
@@ -27,11 +33,28 @@ namespace Common.PipelinesBehaviours
                 var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
                 if (failures.Count > 0)
                 {
+                    if (response is ResultBase res)
+                    {
+                        var error = GetResultErrors(failures);
+                        res.Reasons.Add(error);
+                        return response;
+                    }
                     throw new ValidationException(failures);
                 }
             }
-            
+
             return await next();
+        }
+        private static Error GetResultErrors(List<ValidationFailure> failures)
+        {
+            var errors = failures.Select(e => new Error(e.ErrorMessage));
+            var error = new Error();
+            foreach (var e in errors)
+            {
+                error.Reasons.Add(e);
+            }
+
+            return error;
         }
     }
 }
